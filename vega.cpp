c499e6bd191d0e08ed3786a6d70d7473160b18d6
cppforquants <clemda@protonmail.com>
2025-06-28 22:25:37 +0100

    udpate comment

--- vega.cpp
+++ vega.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
 
-// Compute standard normal PDF without hardcoding pi
+// Compute standard normal PDF
 double normal_pdf(double x) {
     static const double inv_sqrt_2pi = 1.0 / std::sqrt(2.0 * std::acos(-1.0));
     return inv_sqrt_2pi * std::exp(-0.5 * x * x);
